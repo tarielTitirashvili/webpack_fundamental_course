daa0cf96014d6420a0b34f675a837600adcff3dc
tariel <tarieltitirashvili@gmail.com>
2024-05-08 03:49:50 -0700

    test commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import sumTwoParams from "./test"
+
+console.log("Running")
+console.log("sumTwoParams(1,2)", sumTwoParams(1, 2))
---
+++ src/test.js
@@ -0,0 +1,3 @@
+export default function sumTwoParams(param1, param2) {
+  return param1 + param2;
+}
