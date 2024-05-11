e30296028b1e7e22a71061c1d7bebbc5fbd87865
tariel <tarieltitirashvili@gmail.com>
2024-05-11 08:54:14 -0700

    clear from trash

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react"
-import RandomComponent from "./ranomComponent"
 import css from "./app.module.css"
-const style = require("./app.module.css")
 
 type Props = {}
 
 const App = (props: Props) => {
   const [counter, setCounter] = useState<number>(0)
-  console.log("button", css)
+  
   return (
     <div>
       <h3>{counter}</h3>
       <div>
-        <button className={css?.button} onClick={() => setCounter(counter + 1)}>
+        <button className={css.button} onClick={() => setCounter(counter + 1)}>
           increase by one
         </button>
       </div>
-      <RandomComponent />
     </div>
   )
 }
